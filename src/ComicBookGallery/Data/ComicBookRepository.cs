fd0a5b7a6359dc6f5e51547bb9c6665381b63550
Miguel Varona Sarduy <Varonasarduy@gmail.com>
2017-10-20 17:04:42 +0100

    Added comic book list view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -57,6 +57,10 @@ namespace ComicBookGallery.Data
         Favorite = false
     }
           };
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
         public ComicBook GetComicBook(int ID)
         {
             ComicBook comicBookToReturn = null;
